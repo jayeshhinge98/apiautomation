8c31aff86228eb82e82887093d12b21fa0fa392a
jayesh.hinge <jayeshhinge98@gmail.com>
2018-12-02 19:16:39 +0530

    API Automation Demo Project

---
+++ apiautomation/APITestingAutomation/src/main/java/com/api/classes/PhoneNumbers.java
@@ -0,0 +1,13 @@
+package com.api.classes;
+
+public class PhoneNumbers {
+	public String type;
+	public String number;
+	public String getType() {
+		return type;
+	}
+	public String getNumber() {
+		return number;
+	}
+	
+}
